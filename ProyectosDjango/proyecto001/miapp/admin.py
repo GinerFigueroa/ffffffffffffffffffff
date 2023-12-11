9d6179ec41d50da1477fdaf8c6027e3f71645ff0
Giner Figueroa <2113110108@untels.edu.pe>
2023-12-11 10:06:43 -0500

    CRUD CURSOS - Listo

--- ProyectosDjango/proyecto001/miapp/admin.py
+++ ProyectosDjango/proyecto001/miapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Course
 
 # Register your models here.
+
+# Configuración del ModelAdmin para Curso
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('idcourse', 'code', 'name', 'hour', 'credits', 'state')
+    list_filter = ('state',)
+    search_fields = ('code', 'name')
